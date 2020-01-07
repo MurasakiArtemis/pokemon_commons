fc024f8a0e8eec73326e3b99dd7c8cb008b0e5cd
MurasakiArtemis <agarcia@nearsoft.com>
2020-01-07 12:24:56 -0600

    Given pip structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Package used to handle common classes for the pokemon related projects
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pokemon_commons',
+    version='0.1',
+    author="Murasaki Artemis",
+    author_email="murasaki_artemis@outlook.com",
+    description="A package containing pokemon SQLAlchemy classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MurasakiArtemis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
